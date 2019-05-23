1f20828f5a08b28739416e5532b235c9b18007ed
alranna <sleepeatcoderepeat@gmail.com>
2019-05-23 18:05:52 +0500

    Init commit

---
+++ src/main/java/MatrixModules/InputHelper.java
@@ -0,0 +1,42 @@
+package MatrixModules;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputHelper {
+    static protected Scanner in = new Scanner(System.in);
+
+    static public int parserLength() {
+        int n = 0;
+        while (n <= 0) {
+            n = parser("Введено не число или отрицательное число, попробуйте ещё раз \n");
+        }
+
+        return n;
+    }
+
+    static public String input(){
+        return in.nextLine();
+    }
+
+    static public int parser(String Message) {
+        Pattern pattern = Pattern.compile("^(-|\\+)?[0-9]+?$");
+        Matcher matcher;
+        int n = 0;
+        boolean flag = false;
+        String str;
+        while (!flag) {
+            str = input();//input();
+            matcher = pattern.matcher(str);
+            if (matcher.matches()) {
+                n = Integer.valueOf(str);
+                flag = true;
+            } else {
+                System.out.print(Message);//
+            }
+        }
+        return n;
+    }
+
+}
