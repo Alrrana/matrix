3fa899b545f9c047b2cab9b0166c5655bcd9ede2
alranna <sleepeatcoderepeat@gmail.com>
2019-07-22 11:30:25 +0500

    Jackson работает

--- src/test/java/MatrixModules/MatrixTypeDetectorTest.java
+++ src/test/java/MatrixModules/MatrixTypeDetectorTest.java
@@ -9,13 +9,31 @@ class MatrixTypeDetectorTest {
 
     @Test
     void JSONparser() {
-        String input1 = "[{\"value\":\"\"},{\"value\":\"1.5\",\"row\":\"0\",\"col\":\"0\"},{\"value\":\"4\",\"row\":\"1\",\"col\":\"0\"},{\"value\":\"7\",\"row\":\"2\",\"col\":\"0\"},{\"value\":\"10\",\"row\":\"3\",\"col\":\"0\"},{\"value\":\"13\",\"row\":\"4\",\"col\":\"0\"},{\"value\":\"16\",\"row\":\"5\",\"col\":\"0\"},{\"value\":\"2\",\"row\":\"0\",\"col\":\"1\"},{\"value\":\"5\",\"row\":\"1\",\"col\":\"1\"},{\"value\":\"8\",\"row\":\"2\",\"col\":\"1\"},{\"value\":\"11\",\"row\":\"3\",\"col\":\"1\"},{\"value\":\"14\",\"row\":\"4\",\"col\":\"1\"},{\"value\":\"17\",\"row\":\"5\",\"col\":\"1\"},{\"value\":\"3\",\"row\":\"0\",\"col\":\"2\"},{\"value\":\"6\",\"row\":\"1\",\"col\":\"2\"},{\"value\":\"9\",\"row\":\"2\",\"col\":\"2\"},{\"value\":\"12\",\"row\":\"3\",\"col\":\"2\"},{\"value\":\"15\",\"row\":\"4\",\"col\":\"2\"},{\"value\":\"18\",\"row\":\"5\",\"col\":\"2\"}]\n" +
+        String input1 = "[{\"value\":\"1.5\",\"row\":\"0\",\"col\":\"0\"},{\"value\":\"4\",\"row\":\"1\",\"col\":\"0\"},{\"value\":\"7\",\"row\":\"2\",\"col\":\"0\"},{\"value\":\"10\",\"row\":\"3\",\"col\":\"0\"},{\"value\":\"13\",\"row\":\"4\",\"col\":\"0\"},{\"value\":\"16\",\"row\":\"5\",\"col\":\"0\"},{\"value\":\"2\",\"row\":\"0\",\"col\":\"1\"},{\"value\":\"5\",\"row\":\"1\",\"col\":\"1\"},{\"value\":\"8\",\"row\":\"2\",\"col\":\"1\"},{\"value\":\"11\",\"row\":\"3\",\"col\":\"1\"},{\"value\":\"14\",\"row\":\"4\",\"col\":\"1\"},{\"value\":\"17\",\"row\":\"5\",\"col\":\"1\"},{\"value\":\"3\",\"row\":\"0\",\"col\":\"2\"},{\"value\":\"6\",\"row\":\"1\",\"col\":\"2\"},{\"value\":\"9\",\"row\":\"2\",\"col\":\"2\"},{\"value\":\"12\",\"row\":\"3\",\"col\":\"2\"},{\"value\":\"15\",\"row\":\"4\",\"col\":\"2\"},{\"value\":\"18\",\"row\":\"5\",\"col\":\"2\"}]\n" +
                 "1";
-        String input = "[{\"value\":\"\"},{\"value\":\"1\",\"row\":\"0\",\"col\":\"0\"},{\"value\":\"3\",\"row\":\"1\",\"col\":\"0\"},{\"value\":\"5\",\"row\":\"2\",\"col\":\"0\"},{\"value\":\"2\",\"row\":\"0\",\"col\":\"1\"},{\"value\":\"4\",\"row\":\"1\",\"col\":\"1\"},{\"value\":\"6\",\"row\":\"2\",\"col\":\"1\"}]\n";
+        String input = "[{\"value\":\"1\",\"row\":\"0\",\"col\":\"0\"},{\"value\":\"3\",\"row\":\"1\",\"col\":\"0\"},{\"value\":\"5\",\"row\":\"2\",\"col\":\"0\"},{\"value\":\"2\",\"row\":\"0\",\"col\":\"1\"},{\"value\":\"4\",\"row\":\"1\",\"col\":\"1\"},{\"value\":\"6\",\"row\":\"2\",\"col\":\"1\"}]\n";
 
 
-        System.out.println(MatrixTypeDetector.defineType( MatrixTypeDetector.JSONparser(input1)));
-        System.out.println(MatrixTypeDetector.defineType( MatrixTypeDetector.JSONparser(input)));
+        String [][] A =( MatrixTypeDetector.JSONparser(input1));
+        for (String[] i: A
+             ) {
+            for (String item:i
+                 ) {
+                System.out.println(item);
+            }
+
+        }
+        A =( MatrixTypeDetector.JSONparser(input));
+        for (String[] i: A
+        ) {
+            for (String item:i
+            ) {
+                System.out.println(item);
+            }
+
+        }
+
+
     }
 
     @Test
